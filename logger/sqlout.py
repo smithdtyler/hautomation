6f1f42136e49ce78895f222b68180ea1bcf6c565
Tyler Smith <smith.d.tyler@gmail.com>
2014-07-12 07:50:20 -0500

    updated with a try/catch loop to prevent crashing on sql exceptions

--- logger/sqlout.py
+++ logger/sqlout.py
@@ -26,20 +26,23 @@ sensors = [office,hvac,outsidesouth]
 header = 'timestamp,officetemp,hvactemp,outsidesouthtemp'
 print header
 while True:
-	line = '\'' + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '\''
-	for sensor in sensors:
-		s = sensor.read()
-		if s:
-			print s
-			m = re.search('\d{5}', s)
-			tempstr = m.group(0)
-			temp = float(tempstr)
-			tempf = ((9.0/5.0) * (temp/1000)) + 32
-			line = line + ',\'' + str(tempf) + '\''
-		sensor.seek(0)
-	time.sleep(30)
-	statement = 'INSERT INTO `temperature`.`simpletemp_sample` (`id` ,`timestamp`,`officetemp` ,`hvactemp` ,`outsidesouthtemp`)VALUES (NULL,'
-	statement = statement + line
-	statement = statement + ');'
-	print cur.execute(statement)
-	db.commit()
+	try:
+		line = '\'' + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '\''
+		for sensor in sensors:
+			s = sensor.read()
+			if s:
+				print s
+				m = re.search('\d{5}', s)
+				tempstr = m.group(0)
+				temp = float(tempstr)
+				tempf = ((9.0/5.0) * (temp/1000)) + 32
+				line = line + ',\'' + str(tempf) + '\''
+			sensor.seek(0)
+		time.sleep(30)
+		statement = 'INSERT INTO `temperature`.`simpletemp_sample` (`id` ,`timestamp`,`officetemp` ,`hvactemp` ,`outsidesouthtemp`)VALUES (NULL,'
+		statement = statement + line
+		statement = statement + ');'
+		print cur.execute(statement)
+		db.commit()
+	except:
+		print "exception!"
