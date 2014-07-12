8e883a6faea2af2182d5326021dfea2f3c75adde
Tyler Smith <smith.d.tyler@gmail.com>
2014-07-12 07:59:07 -0500

    updated graph with all available feeds

--- server/temperature/simpletemp/views.py
+++ server/temperature/simpletemp/views.py
@@ -5,7 +5,7 @@ from models import Sample
 from django.shortcuts import render
 
 def index(request):
-    latest_sample_list = Sample.objects.all().order_by('-timestamp')[:50]
+    latest_sample_list = Sample.objects.all().order_by('-timestamp')[:120]
     context = {'latest_sample_list': latest_sample_list}
     return render(request, 'simpletemp/index.html', context)
 
