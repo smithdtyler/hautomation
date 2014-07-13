b7c1548317165d368af866afb3182eb0d98b863b
Tyler Smith <smith.d.tyler@gmail.com>
2014-07-12 22:13:03 -0500

    added second graph

--- server/temperature/simpletemp/views.py
+++ server/temperature/simpletemp/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def index(request):
     latest_sample_list = Sample.objects.all().order_by('-timestamp')[:120]
-    context = {'latest_sample_list': latest_sample_list}
+    last_twentyfour_hours_list = Sample.objects.all().order_by('-timestamp')[:2880]
+    context = {'latest_sample_list': latest_sample_list, 'last_twentyfour_hours_list': last_twentyfour_hours_list}
     return render(request, 'simpletemp/index.html', context)
 
