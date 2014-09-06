efc15b32871097e86580462c7045898e5b03c09c
Tyler Smith <smith.d.tyler@gmail.com>
2014-09-06 10:13:37 -0500

    added garage sensor

--- logger/sqlout.py
+++ logger/sqlout.py
@@ -21,9 +21,10 @@ print 'opening..'
 office = open('/sys/devices/w1_bus_master1/28-000005ab9297/w1_slave','r')
 hvac = open('/sys/devices/w1_bus_master1/28-000005abeae1/w1_slave','r')
 outsidesouth = open('/sys/devices/w1_bus_master1/28-000005aad85d/w1_slave','r')
+garage = open('/sys/devices/w1_bus_master/28-000005ab940d/w1_slave','r')
 
-sensors = [office,hvac,outsidesouth]
-header = 'timestamp,officetemp,hvactemp,outsidesouthtemp'
+sensors = [office,hvac,outsidesouth,garage]
+header = 'timestamp,officetemp,hvactemp,outsidesouthtemp, garagetemp'
 print header
 while True:
 	try:
@@ -39,7 +40,7 @@ while True:
 				line = line + ',\'' + str(tempf) + '\''
 			sensor.seek(0)
 		time.sleep(30)
-		statement = 'INSERT INTO `temperature`.`simpletemp_sample` (`id` ,`timestamp`,`officetemp` ,`hvactemp` ,`outsidesouthtemp`)VALUES (NULL,'
+		statement = 'INSERT INTO `temperature`.`simpletemp_sample` (`id` ,`timestamp`,`officetemp` ,`hvactemp` ,`outsidesouthtemp`,`garagetemp`)VALUES (NULL,'
 		statement = statement + line
 		statement = statement + ');'
 		print cur.execute(statement)
